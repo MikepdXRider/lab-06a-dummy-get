a38807ea2cc55c85b08995896f03de3ea4946d62
Michael Rider <mikepdxrider@gmail.com>
2021-09-28 13:09:04 -0700

    updates endpoints in index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { dataArr } = require('./data.js')
 // imports express library
 const express = require('express')
 // calls express and saves return to app variable. 
@@ -12,9 +13,15 @@ const port = process.env.PORT || 3000
 // Allows other servers to hit our endpoints. 
 app.use(cors())
 
+// Defines /teas endpoint, if hit it will return the entire dataArr.
+app.get('/teas', (req, res) => {
+    res.json(dataArr)
+})
 
-app.get('/', (req, res) => {s
-  res.send('Hello World!')
+// Defines /tea/:tea endpoint, if hit will find correct tea and return the dataObj.
+app.get('/teas/:tea', (req, res) => {
+    const matchingTea = dataArr.find(dataObj => dataObj.tea === req.params.tea)
+    res.json(matchingTea)
 })
 
 app.listen(port, () => {
