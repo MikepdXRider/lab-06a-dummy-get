ff92710ff0644436d23e7badca655625bce03f75
Michael Rider <mikepdxrider@gmail.com>
2021-09-28 13:35:02 -0700

    abstracts server.js

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
-const { dataArr } = require('./data.js')
-// imports express library
-const express = require('express')
-// calls express and saves return to app variable. 
-const app = express()
-// imports cors to enable remote servers access to our backend.
-const cors = require('cors')
-// imports and configures dotenv.
-require('dotenv').config()
+// import app variable from 
+const { app } = require('./server')
+
 // first attempts to retrieve a port hidden in .env file, if none exist or not accessible, return default port #3000
 const port = process.env.PORT || 3000
-
-// Allows other servers to hit our endpoints. 
-app.use(cors())
-
-// Defines /teas endpoint, if hit it will return the entire dataArr.
-app.get('/teas', (req, res) => {
-    res.json(dataArr)
-})
-
-// Defines /tea/:tea endpoint, if hit will find correct tea and return the dataObj.
-app.get('/teas/:id', (req, res) => {
-    const matchingTea = dataArr.find(dataObj => dataObj.id === Number(req.params.id))
-    res.json(matchingTea)
-})
 
 // On deploy, communicates to terminal where we can view our content.
 app.listen(port, () => {
